8ada3c6dbda3a9b1e0171f0cf2874de80e0b4a7a
Samantha Bartlett <sammynilla@protonmail.com>
2022-05-23 01:06:33 -0400

    abstracted some parts of the code. converted chimmy texture to png. base iteration of frame limiting via wait/deltatime

---
+++ base_types.h
@@ -0,0 +1,12 @@
+#define global static
+#define internal static
+#define local_persist static
+
+typedef int i32;
+typedef i32 b32;
+
+typedef i32     b32;
+typedef float   r32;
+typedef double  r64;
+
+#define InvalidDefaultCase default:break
--- chimmy.c
+++ chimmy.c
@@ -1,13 +1,6 @@
-#include "raylib.h"
+#include "base_types.h"
 
-#define global static
-#define internal static
-#define local_persist static
-
-typedef int i32;
-typedef i32 b32;
-typedef float r32;
-typedef double r64;
+#include <raylib.h>
 
 #define BASE_SCREEN_WIDTH 200
 #define BASE_SCREEN_HEIGHT 150
@@ -16,9 +9,7 @@ global r32 screen_scale_modifier = 2;
 global r32 screen_scale_min = 1;
 global r32 screen_scale_max = 4;
 
-typedef enum GameScene { LOGO = 0, OPTIONS, GAMEPLAY, ENDING } gameScene;
-
-#define InvalidDefaultCase default:break
+typedef enum GameScene { LOGO = 0, OPTIONS, GAMEPLAY } gameScene;
 
 /* https://colors.artyclick.com/color-name-finder/ */
 /* Prefixed all the colors with PAL_ to prevent name collisions with raylib */
@@ -31,6 +22,9 @@ enum PALETTE {
   PAL_BARNEY_PURPLE = 40
 };
 
+#define PIXEL_SIZE  1
+#define CHIMMY_SIZE 20
+
 void
 WindowScaledResize(void) {
   SetWindowSize(BASE_SCREEN_WIDTH * (i32)screen_scale_modifier,
@@ -40,27 +34,41 @@ WindowScaledResize(void) {
 int
 main(void)
 {
-  /* SetConfigFlags(FLAG_MSAA_4X_HINT); */
+  /* SetConfigFlags(FLAG_VSYNC_HINT); */
   InitWindow(BASE_SCREEN_WIDTH, BASE_SCREEN_HEIGHT, "chimmy");
+  SetExitKey(KEY_NULL);
   WindowScaledResize();
 
-  SetTargetFPS(60);
-  SetExitKey(KEY_NULL);
+  r64 previousTime = GetTime();
+  r64 currentTime = 0.0;
+  r64 updateDrawTime = 0.0;
+  r64 waitTime = 0.0;
+  r32 deltaTime = 0.0f;
+
+  r32 timeCounter = 0.0f;
+
+  int targetFPS = 60;
 
   gameScene scene = LOGO;
-  i32 map_color = 8;
+  i32 map_color = PAL_WHITE;
+
+  Texture2D chimmy_sprite = LoadTexture("data/image/chimmy.png");
+  Rectangle chimmy_anim = { 0, 0, CHIMMY_SIZE, CHIMMY_SIZE };
+  Rectangle chimmy_rect = { 0, 0, CHIMMY_SIZE, CHIMMY_SIZE };
 
   Texture2D map_bg = LoadTexture("data/image/palette.png");
-  Rectangle palette_point = { 0, 0, 1, 1 };
+  Rectangle palette_point = { 0, 0, PIXEL_SIZE, PIXEL_SIZE };
   Rectangle screen_rect = { 0, 0, BASE_SCREEN_WIDTH, BASE_SCREEN_HEIGHT };
 
   while (!WindowShouldClose())
   {
+    PollInputEvents();
+
     switch (scene) {
       case LOGO: {
         if (IsKeyPressed(KEY_ENTER)) {
-          palette_point.x = (r32)(map_color % 8);
-          palette_point.y = (r32)(map_color / (r32)8);
+          palette_point.x = (r32)((i32)map_color % 8);
+          palette_point.y = (r32)(map_color / 8);
           scene = GAMEPLAY;
         }
         if (IsKeyPressed(KEY_ESCAPE)) { scene = OPTIONS; }
@@ -82,21 +90,41 @@ main(void)
       InvalidDefaultCase;
     }
 
+    timeCounter += deltaTime;
+
     BeginDrawing();
       ClearBackground(RAYWHITE);
       switch (scene) {
         case OPTIONS: {
-          DrawText("Options:", 5, 5, 10, BLACK);
-          DrawText("TEST", 550, 550, 10, BLACK);
+          DrawText("Options:", 50, 125, 10, BLACK);
         }break;
         case GAMEPLAY: {
           Vector2 origin = { 0, 0 };
+          screen_rect.width = BASE_SCREEN_WIDTH * screen_scale_modifier;
+          screen_rect.height = BASE_SCREEN_HEIGHT * screen_scale_modifier;
+          chimmy_rect.x = chimmy_rect.y = 32;
+          chimmy_rect.width = CHIMMY_SIZE * screen_scale_modifier;
+          chimmy_rect.height = CHIMMY_SIZE * screen_scale_modifier;
           DrawTexturePro(map_bg, palette_point, screen_rect, origin, 0, WHITE);
-          DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 10, BLACK);
+          DrawTexturePro(chimmy_sprite, chimmy_anim, chimmy_rect, origin, 0, WHITE);
         }break;
         InvalidDefaultCase;
       }
+      DrawFPS(10, 10);
     EndDrawing();
+
+    currentTime = GetTime();
+    updateDrawTime = currentTime - previousTime;
+    waitTime = (1.0f/(r32)targetFPS) - updateDrawTime;
+    if (waitTime > 0.0) {
+      WaitTime((r32)waitTime * 1000.f);
+      currentTime = GetTime();
+      deltaTime = (r32)(currentTime - previousTime);
+    }
+    else {
+      deltaTime = (r32)updateDrawTime;
+    }
+    previousTime = currentTime;
   }
 
   UnloadTexture(map_bg);
